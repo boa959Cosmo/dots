271c00cf250a55882f577a62faabb1a9166894e6
Ferrari Noah Cosmo <ferrn2@bfh.ch>
2024-06-10 05:32:03 +0200

    foot config

---
+++ brick/copyConfig.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#copy files from home over here
+
+#hypr
+ cp -R ~/.config/hypr/. .config/hypr
+ 
+#waybar
+ cp -R ~/.config/waybar/. .config/waybar
+
+#wofi
+ cp -R ~/.config/wofi/. .config/wofi
+
+#mako
+ cp -R ~/.config/mako/. .config/mako
+
+#fontconfig
+ cp -R ~/.config/fontconfig/. .config/fontconfig
+
+#zshrc
+ cp -R ~/.zshrc zshrc
+
+#lf 
+ cp -R ~/.config/lf/. .config/lf
+
+#alacritty
+# cp -R ~/.config/alacritty/. .config/alacritty
+
+#Wallpapers
+ cp -R ~/Pictures/Wallpapers/. Pictures
+
+ #foot
+  cp -R ~/.config/foot/. .config/foot
