fedd64d308e8bed0000495cdd1b105588b0c991d
Ferrari Noah Cosmo <ferrn2@bfh.ch>
2026-04-07 17:59:36 +0200

    frühlings putz

--- brick/copyConfig.sh
+++ brick/copyConfig.sh
@@ -3,31 +3,31 @@
 #copy files from home over here
 
 #hypr
- cp -R ~/.config/hypr/. .config/hypr
+  cp -R ~/.config/hypr/. config/hypr
  
 #waybar
- cp -R ~/.config/waybar/. .config/waybar
+  cp -R ~/.config/waybar/. config/waybar
 
 #wofi
- cp -R ~/.config/wofi/. .config/wofi
+  cp -R ~/.config/wofi/. config/wofi
 
 #mako
- cp -R ~/.config/mako/. .config/mako
+  cp -R ~/.config/mako/. config/mako
 
 #fontconfig
- cp -R ~/.config/fontconfig/. .config/fontconfig
+  cp -R ~/.config/fontconfig/. config/fontconfig
 
 #zshrc
- cp -R ~/.zshrc zshrc
+  cp -R ~/.zshrc zshrc
 
-#lf 
- cp -R ~/.config/lf/. .config/lf
-
-#alacritty
-# cp -R ~/.config/alacritty/. .config/alacritty
+#yazi
+  cp -R ~/.config/yazi/. config/yazi
 
 #Wallpapers
- cp -R ~/Pictures/Wallpapers/. Pictures
+  cp -R ~/Pictures/Wallpapers/. Pictures
 
- #foot
-  cp -R ~/.config/foot/. .config/foot
+#foot
+  cp -R ~/.config/foot/. config/foot
+
+#mimelist used with handlr-regex
+  cp -R ~/.config/mimeapps.list config/mimeapps.list
